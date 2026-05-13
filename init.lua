29edc888b9deccca6e973b55c5e87717476825cf
Sasha <Filimonov_av@list.ru>
2026-05-13 12:17:50 +0500

    Добавил работу с системным буфером. До этого работало только внутри tmux (он был провайдером), а нативно не работало. Щас вроде рабоатет (в контейнере тоже)

--- init.lua
+++ init.lua
@@ -2,3 +2,15 @@
 require("core.options")  -- загрузить настройки UI и поведения редактора
 require("core.keymaps")  -- загрузить пользовательские клавиатурные привязки
 require("core.lazy")  -- инициализировать lazy.nvim (плагин‑менеджер)
+
+vim.g.clipboard = {
+  name = 'OSC 52',
+  copy = {
+    ['+'] = require('vim.ui.clipboard.osc52').copy('+'),
+    ['*'] = require('vim.ui.clipboard.osc52').copy('*'),
+  },
+  paste = {
+    ['+'] = require('vim.ui.clipboard.osc52').paste('+'),
+  },
+}
+vim.opt.clipboard:append('unnamedplus')
