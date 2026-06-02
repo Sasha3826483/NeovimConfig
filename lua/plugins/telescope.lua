d14cfdea485856ad96a40c05f5560d5267589d39
Sasha <Filimonov_av@list.ru>
2026-06-02 15:23:06 +0500

    Добавил навигацию по ФС с помощью расширения для telescope: file_browser

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,8 +1,9 @@
--- telescope.lua: конфигурирует nvim-telescope (поиск файлов, live grep, буферы, help)
+-- telescope.lua
 return {
-  "nvim-telescope/telescope.nvim",  -- мощный поиск файлов и многое другое
+  "nvim-telescope/telescope.nvim",
   dependencies = {
     "nvim-lua/plenary.nvim",
+    "nvim-telescope/telescope-file-browser.nvim",
   },
 
   config = function()
@@ -10,20 +11,31 @@ return {
 
     telescope.setup({
       defaults = {
-        prompt_prefix = "🔍 ",  -- префикс ввода поиска
-        selection_caret = "➜ ",     -- маркер выбранного элемента
+        prompt_prefix = "🔍 ",
+        selection_caret = "➜ ",
         layout_config = {
           horizontal = { preview_width = 0.6 },
         },
       },
     })
 
+    telescope.load_extension("file_browser")
+
     local builtin = require("telescope.builtin")
 
-    -- удобные сочетания клавиш для разных режимов поиска
     vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Find files" })
     vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Live grep" })
     vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Buffers" })
     vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Help tags" })
+    
+    vim.keymap.set("n", "<leader>fbrowser", function()
+      require("telescope").extensions.file_browser.file_browser()
+    end, { desc = "File Browser" })
+    
+    vim.keymap.set("n", "<leader>fbr", function()
+      require("telescope").extensions.file_browser.file_browser({
+        path = "%:p:h",
+      })
+    end, { desc = "File Browser from current file" })
   end,
 }
